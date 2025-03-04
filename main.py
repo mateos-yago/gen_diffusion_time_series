0a3b4106a435610d464a4484bf805d04569e915e
ymateos <yagomateos@gmail.com>
2025-03-04 10:58:11 -0500

    .

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import ddpm_ar1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def main():
+    ddpm_ar1.train_and_plot()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
