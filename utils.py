622fabe9d7d2a2e12949607858155c4366ee4cfe
ymateos <yagomateos@gmail.com>
2025-03-26 17:47:47 -0400

    [ym] added parameter estimator function to utils

--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
 import matplotlib.pyplot as plt
 
+def AR_1_phi_parameter_estimation(series):
+    """This function takes an array of AR1 time series as an argument
+    and returns  a list of the phi1 estimated parameter for each series"""
+    params_list=[]
+    for serie in series:
+        tsmodel=ar_model.AutoReg(serie, 1, trend='n')
+        result=tsmodel.fit()
+        params_list.append(result.params[0])
+    return params_list
